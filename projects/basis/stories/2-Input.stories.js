6a4a0280558365a0e454fc985668af730c8afc46
yoichi kobayashi <info@tplh.net>
2019-10-09 17:53:02 +0900

    added the story section for multiple checkbox.

--- projects/basis/stories/2-Input.stories.js
+++ projects/basis/stories/2-Input.stories.js
@@ -46,28 +46,41 @@ export const checkboxHasLabel = () => ({
   },
   data: function() {
     return {
-      checkState: 0,
+      items: [
+        { id: 'itemA', value: 'itemA', label: 'check itemA', state: 0, },
+        { id: 'itemB', value: 'itemB', label: 'check itemB', state: 0, },
+        { id: 'itemC', value: 'itemC', label: 'check itemC', state: 0, },
+        { id: 'itemD', value: 'itemD', label: 'check itemD', state: 0, },
+        { id: 'itemE', value: 'itemE', label: 'check itemE', state: 0, },
+      ],
     }
   },
   template: `
     <div>
-      <div>checkState: {{ showCheckState }}</div>
-      <CheckboxHasLabel :state='checkState' id='checkbox' @click="click" />
+      <div>
+        selected: {{ showSelectedItems }}
+      </div>
+      <CheckboxHasLabel
+        v-for = 'item, index in items'
+        :state = 'item.state'
+        @click = 'click(index)'
+      >
+        {{ item.label }}
+      </CheckboxHasLabel>
     </div>
   `,
   computed: {
-    showCheckState: function() {
-      switch (this.checkState) {
-        case 0: return '0: not selected';
-        case 1: return '1: selected all';
-        case 2: return '2: selected part';
-        default: return;
+    showSelectedItems: function() {
+      let result = [];
+      for (var i = 0; i < this.items.length; i++) {
+        if (this.items[i].state === 1) result.push(this.items[i].value);
       }
+      return result.join(', ');
     },
   },
   methods: {
-    click: function() {
-      this.checkState = (this.checkState + 1) % 3;
+    click: function(i) {
+      this.items[i].state = (this.items[i].state + 1) % 2;
     },
   },
 });
