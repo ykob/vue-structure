996884fa6342352ce295a6d801540b61a5d533f3
yoichi kobayashi <koba@tsumikiinc.com>
2019-10-10 15:26:19 +0900

    clean up.

--- projects/basis/stories/2-Input.stories.js
+++ projects/basis/stories/2-Input.stories.js
@@ -1,6 +1,3 @@
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-
 import Checkbox from '~/02/Checkbox.vue';
 import MultipleCheckbox from '~/02/MultipleCheckbox.vue';
 
