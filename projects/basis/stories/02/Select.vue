f166dc68ba386f0f26a1f5aa55c6d48e92b25907
yoichi kobayashi <info@tplh.net>
2019-10-12 23:59:20 +0900

    made the new story.

---
+++ projects/basis/stories/02/Select.vue
@@ -0,0 +1,42 @@
+<script>
+  import Select from '@/components/atoms/Select.vue';
+
+  export default {
+    components: {
+      Select,
+    },
+    data: function() {
+      return {
+        selected: 'itemA',
+        items: [
+          { value: 'itemA', label: 'select itemA', },
+          { value: 'itemB', label: 'select itemB', },
+          { value: 'itemC', label: 'select itemC', },
+          { value: 'itemD', label: 'select itemD', },
+          { value: 'itemE', label: 'select itemE', },
+        ],
+      }
+    },
+    computed: {
+    },
+    methods: {
+      change: function(e) {
+        this.selected = e.currentTarget.value;
+      },
+    },
+  };
+</script>
+
+<template lang="pug">
+  div
+    div
+      |selected: {{ selected }}
+    Select(
+      :selected = 'selected'
+      :items = 'items'
+      @change = 'change'
+      )
+</template>
+
+<style lang="scss" scope>
+</style>
--- projects/basis/stories/2-Input.stories.js
+++ projects/basis/stories/2-Input.stories.js
@@ -1,6 +1,7 @@
 import Checkbox from '~/02/Checkbox.vue';
 import MultipleCheckbox from '~/02/MultipleCheckbox.vue';
 import Radio from '~/02/Radio.vue';
+import Select from '~/02/Select.vue';
 
 export default {
   title: 'Input',
@@ -9,3 +10,4 @@ export default {
 export const checkbox = () => Checkbox;
 export const multipleCheckbox = () => MultipleCheckbox;
 export const radio = () => Radio;
+export const select = () => Select;
