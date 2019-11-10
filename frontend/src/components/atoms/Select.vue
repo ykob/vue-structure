1bf1d0bd8cdc30c74a608a0e0fe5899bb79f8f16
yoichi kobayashi <info@tplh.net>
2019-11-10 23:55:25 +0900

    update the select element component.

--- frontend/src/components/atoms/Select.vue
+++ frontend/src/components/atoms/Select.vue
@@ -17,18 +17,50 @@
 </script>
 
 <template lang="pug">
-  select.c-select(
-    name = ''
-    @change = 'change'
-    )
-    option(
-      v-for = 'item in items'
-      :value = 'item.value'
+  .c-select
+    select(
+      :name = 'name'
+      @change = 'change'
       )
-      |{{ item.label }}
+      option(
+        v-for = 'item in items'
+        :value = 'item.value'
+        )
+        |{{ item.label }}
+    .c-select__icon
 </template>
 
 <style lang="scss">
   .c-select {
+    display: inline-block;
+    position: relative;
+    select {
+      height: 32px;
+      appearance: none;
+      -webkit-appearance: none;
+      box-sizing: border-box;
+      padding-right: 32px;
+      padding-left: 16px;
+      border: 1px solid map-get($colors, 'wb-light-m2');
+      border-radius: 4px;
+      &:focus {
+        outline: 0;
+        border-color: map-get($colors, 'info-medium');
+      }
+      &:disabled {
+        color: map-get($colors, 'wb-medium');
+        background-color: map-get($colors, 'wb-light');
+      }
+      &.has-error {
+        border-color: map-get($colors, 'error-medium');
+        background-color: map-get($colors, 'error-light');
+      }
+    }
+    &__icon {
+      position: absolute;
+      top: 4px;
+      right: 8px;
+      pointer-events: none;
+    }
   }
 </style>
